bbe2ad540051be367de8079323ccd95e9eb88777
Matthias Geuchen <mgeuchen@mail.upb.de>
2020-11-13 16:50:48 +0100

    Add resetLedger transaction
    Co-authored-by: Niko Bergemann <niko.bergemann@web.de>
    Co-authored-by: Marie-Christin Oczko <mco33415@mail.uni-paderborn.de>
    Co-authored-by: Steffen Luis <sluis1@mail.uni-paderborn.de>

--- UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/ContractBase.java
+++ UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/ContractBase.java
@@ -1,8 +1,16 @@
 package de.upb.cs.uc4.chaincode;
 
+import de.upb.cs.uc4.chaincode.util.*;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public abstract class ContractBase implements ContractInterface {
@@ -17,5 +25,24 @@ public abstract class ContractBase implements ContractInterface {
     @Transaction()
     public String getVersion(final Context ctx) {
         return version;
+    }
+
+    @Transaction()
+    public String resetLedger(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+        if (!ctx.getClientIdentity().getAttributeValue("hf.Type").equals("admin")) {
+            return "FAILURE";
+        }
+        List<ContractUtil> cUtils = Arrays.asList(
+                new ApprovalContractUtil(),
+                new CertificateContractUtil(),
+                new ExaminationRegulationContractUtil(),
+                new MatriculationDataContractUtil());
+        for (ContractUtil cUtil: cUtils) {
+            for (KeyValue keyValue: cUtil.getAllRawStates(stub)) {
+                stub.delState(keyValue.getKey());
+            }
+        }
+        return "";
     }
 }
