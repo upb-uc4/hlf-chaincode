ec88436555797d67404a8aaa49c27b77fedf10b2
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-11-30 16:28:36 +0100

    additional Tests for AddAdmission

--- UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/AdmissionContractTest.java
+++ UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/AdmissionContractTest.java
@@ -1,71 +1,38 @@
 package de.upb.cs.uc4.chaincode;
 
-
-import com.google.gson.reflect.TypeToken;
 import de.upb.cs.uc4.chaincode.mock.MockChaincodeStub;
 import de.upb.cs.uc4.chaincode.model.*;
 import de.upb.cs.uc4.chaincode.util.AdmissionContractUtil;
 import de.upb.cs.uc4.chaincode.util.GsonWrapper;
-import de.upb.cs.uc4.chaincode.util.MatriculationDataContractUtil;
 import de.upb.cs.uc4.chaincode.util.TestUtil;
 import org.hyperledger.fabric.contract.Context;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.Executable;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public final class AdmissionContractTest {
+public final class AdmissionContractTest extends TestCreationBase {
 
     private final AdmissionContract contract = new AdmissionContract();
     private final AdmissionContractUtil cUtil = new AdmissionContractUtil();
 
-    @TestFactory
-    List<DynamicTest> createTests() {
-        String testConfigDir = "src/test/resources/test_configs/admission_contract";
-        File dir = new File(testConfigDir);
-        File[] testConfigs = dir.listFiles();
+    DynamicTest CreateTest(JsonIOTest test){
+        String testType = test.getType();
+        String testName = test.getName();
+        JsonIOTestSetup setup = test.getSetup();
+        List<String> input = TestUtil.toStringList(test.getInput());
+        List<String> compare = TestUtil.toStringList(test.getCompare());
+        List<Approval> ids = test.getIds();
 
-        List<JsonIOTest> testConfig;
-        Type type = new TypeToken<List<JsonIOTest>>() {}.getType();
-        ArrayList<DynamicTest> tests = new ArrayList<>();
-
-        if (testConfigs == null) {
-            throw new RuntimeException("No test configurations found.");
+        switch (testType) {
+            case "addAdmission_SUCCESS":
+                return DynamicTest.dynamicTest(testName, addAdmissionSuccessTest(setup, input, compare, ids));
+            case "addAdmission_Failure":
+                return DynamicTest.dynamicTest(testName, addAdmissionFailureTest(setup, input, compare, ids));
+            default:
+                throw new RuntimeException("Test " + testName + " of type " + testType + " could not be matched.");
         }
-
-        for (File file: testConfigs) {
-            try {
-                testConfig = GsonWrapper.fromJson(new FileReader(file.getPath()), type);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                return null;
-            }
-
-            for (JsonIOTest test : testConfig) {
-                JsonIOTestSetup setup = test.getSetup();
-                List<String> input = TestUtil.toStringList(test.getInput());
-                List<String> compare = TestUtil.toStringList(test.getCompare());
-                switch (test.getType()) {
-                    case "addAdmission_SUCCESS":
-                        tests.add(DynamicTest.dynamicTest(
-                                test.getName(),
-                                addAdmissionSuccessTest(setup, input, compare, test.getIds())
-                        ));
-                        break;
-                    default:
-                        throw new RuntimeException("Test " + test.getName() + " of type " + test.getType() + " could not be matched.");
-                }
-            }
-        }
-        return tests;
     }
 
     private Executable addAdmissionSuccessTest(
@@ -76,18 +43,39 @@ public final class AdmissionContractTest {
     ) {
         return () -> {
             MockChaincodeStub stub = TestUtil.mockStub(setup);
-            /*
+            /* TODO: approval handling
             ApprovalContract approvalContract = new ApprovalContract();
             for (Approval id: ids) {
                 Context ctx = TestUtil.mockContext(stub, id);
                 approvalContract.approveTransaction(ctx, contract.contractName,"addAdmission", input.get(0));
             }*/
             Context ctx = TestUtil.mockContext(stub);
-            assertThat(contract.addAdmission(ctx, input.get(0), input.get(1), input.get(2), input.get(3)))
-                    .isEqualTo(compare.get(0));
+            String addResult = contract.addAdmission(ctx, input.get(0), input.get(1), input.get(2), input.get(3));
+            assertThat(addResult).isEqualTo(compare.get(0));
+
             Admission compareAdmission = GsonWrapper.fromJson(compare.get(0), Admission.class);
-            assertThat(cUtil.getStringState(ctx.getStub(), compareAdmission.getAdmissionId()))
-                    .isEqualTo(compare.get(0));
+            Admission ledgerAdmission = cUtil.getState(stub, compareAdmission.getAdmissionId(), Admission.class);
+            assertThat(ledgerAdmission).isEqualTo(compareAdmission);
+        };
+    }
+
+    private Executable addAdmissionFailureTest(
+            JsonIOTestSetup setup,
+            List<String> input,
+            List<String> compare,
+            List<Approval> ids
+    ) {
+        return () -> {
+            MockChaincodeStub stub = TestUtil.mockStub(setup);
+            /* TODO: approval handling
+            ApprovalContract approvalContract = new ApprovalContract();
+            for (Approval id: ids) {
+                Context ctx = TestUtil.mockContext(stub, id);
+                approvalContract.approveTransaction(ctx, contract.contractName,"addAdmission", input.get(0));
+            }*/
+            Context ctx = TestUtil.mockContext(stub);
+            String result =  contract.addAdmission(ctx, input.get(0), input.get(1), input.get(2), input.get(3));
+            assertThat(result).isEqualTo(compare.get(0));
         };
     }
 }
---
+++ UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/TestCreationBase.java
@@ -0,0 +1,49 @@
+package de.upb.cs.uc4.chaincode;
+
+
+import com.google.gson.reflect.TypeToken;
+import de.upb.cs.uc4.chaincode.model.JsonIOTest;
+import de.upb.cs.uc4.chaincode.util.GsonWrapper;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class TestCreationBase {
+
+    abstract DynamicTest CreateTest(JsonIOTest test);
+
+    @TestFactory
+    List<DynamicTest> createTests() {
+        String testConfigDir = "src/test/resources/test_configs/admission_contract";
+        File dir = new File(testConfigDir);
+        File[] testConfigs = dir.listFiles();
+
+        List<JsonIOTest> testConfig;
+        Type type = new TypeToken<List<JsonIOTest>>() {}.getType();
+        ArrayList<DynamicTest> tests = new ArrayList<>();
+
+        if (testConfigs == null) {
+            throw new RuntimeException("No test configurations found.");
+        }
+
+        for (File file: testConfigs) {
+            try {
+                testConfig = GsonWrapper.fromJson(new FileReader(file.getPath()), type);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            for (JsonIOTest test : testConfig) {
+                tests.add(CreateTest(test));
+            }
+        }
+        return tests;
+    }
+}
