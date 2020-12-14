7207c20e57bb7f41ba6b15d1818556d7e9582b0e
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-12-14 15:59:07 +0100

    fix compile err for new contract

--- UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/GroupContract.java
+++ UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/GroupContract.java
@@ -4,7 +4,7 @@ import de.upb.cs.uc4.chaincode.exceptions.LedgerAccessError;
 import de.upb.cs.uc4.chaincode.model.Group;
 import de.upb.cs.uc4.chaincode.model.errors.InvalidParameter;
 import de.upb.cs.uc4.chaincode.util.GroupContractUtil;
-import de.upb.cs.uc4.chaincode.util.GsonWrapper;
+import de.upb.cs.uc4.chaincode.util.helper.GsonWrapper;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.annotation.Contract;
 import org.hyperledger.fabric.contract.annotation.Default;
