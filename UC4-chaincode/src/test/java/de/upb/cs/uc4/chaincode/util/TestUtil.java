3a6bc4642a2ac4eacff33f4cae96632ecdfc85e8
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-01-29 17:36:56 +0100

    fix missing import

--- UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/util/TestUtil.java
+++ UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/util/TestUtil.java
@@ -1,14 +1,13 @@
 package de.upb.cs.uc4.chaincode.util;
 
-import de.upb.cs.uc4.chaincode.contract.certificate.CertificateContract;
 import de.upb.cs.uc4.chaincode.contract.operation.OperationContract;
+import de.upb.cs.uc4.chaincode.helper.AccessManager;
 import de.upb.cs.uc4.chaincode.helper.GsonWrapper;
 import de.upb.cs.uc4.chaincode.mock.MockChaincodeStub;
 import de.upb.cs.uc4.chaincode.model.Dummy;
 import de.upb.cs.uc4.chaincode.model.JsonIOTestSetup;
 import org.hyperledger.fabric.contract.ClientIdentity;
 import org.hyperledger.fabric.contract.Context;
-import org.hyperledger.fabric.shim.ChaincodeStub;
 
 import java.util.ArrayList;
 import java.util.List;
