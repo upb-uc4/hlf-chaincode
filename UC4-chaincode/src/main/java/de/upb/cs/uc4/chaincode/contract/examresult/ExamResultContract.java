94e9844eecfeb0b83378a0c0ca074a9c04f095eb
Marie-Christin H. Oczko <mco33415@mail.uni-paderborn.de>
2021-02-02 11:04:44 +0100

    start working on examResult contract

--- UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/contract/examresult/ExamResultContract.java
+++ UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/contract/examresult/ExamResultContract.java
@@ -1,10 +1,16 @@
 package de.upb.cs.uc4.chaincode.contract.examresult;
 
 import de.upb.cs.uc4.chaincode.contract.ContractBase;
-import de.upb.cs.uc4.chaincode.contract.group.GroupContractUtil;
-import org.hyperledger.fabric.contract.Context;
+import de.upb.cs.uc4.chaincode.exceptions.SerializableError;
+import de.upb.cs.uc4.chaincode.helper.HyperledgerManager;
 import org.hyperledger.fabric.contract.annotation.Contract;
+import de.upb.cs.uc4.chaincode.model.ExamResult;
+import de.upb.cs.uc4.chaincode.model.ExamResultEntry;
+import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.annotation.Transaction;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Contract(
         name = ExamResultContract.contractName
@@ -16,5 +22,34 @@ public class ExamResultContract extends ContractBase {
     public final static String contractName = "UC4.ExamResult";
     public final static String transactionNameAddExamResult = "addExamResult";
     public final static String transactionNameGetExamResultEntries= "getExamResultEntries";
+
+    /**
+     * Adds an examResult.
+     *
+     * @param ctx          transaction context providing access to ChaincodeStub etc.
+     * @param examResult    examResult to be added
+     * @return examResult object on ledger on success, serialized error on failure
+     */
+    @Transaction()
+    public String addExamResult(final Context ctx, ExamResult examResult) {
+        String transactionName = HyperledgerManager.getTransactionName(ctx.getStub());
+        return "";
+    }
+
+    /**
+     * Gets ExamResultEntries from the ledger.
+     *
+     * @param ctx          transaction context providing access to ChaincodeStub etc.
+     * @param enrollmentId enrollmentId
+     * @param examIds      examIds
+     * @return the full list of existing ExamResultEntries matching the filter parameters or an empty list of none could be found
+     */
+
+    @Transaction()
+    public String getExamResultEntries(final Context ctx, String enrollmentId, List<String> examIds) {
+        String transactionName = HyperledgerManager.getTransactionName(ctx.getStub());
+
+        return "";
+    }
 
 }
--- UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/contract/examresult/ExamResultContractUtil.java
+++ UC4-chaincode/src/main/java/de/upb/cs/uc4/chaincode/contract/examresult/ExamResultContractUtil.java
@@ -1,12 +1,26 @@
 package de.upb.cs.uc4.chaincode.contract.examresult;
 
 import de.upb.cs.uc4.chaincode.contract.ContractUtil;
+import de.upb.cs.uc4.chaincode.exceptions.serializable.ParameterError;
+import org.hyperledger.fabric.contract.Context;
+
+import java.util.List;
 
 public class ExamResultContractUtil extends ContractUtil {
 
     public ExamResultContractUtil() {
-        keyPrefix = "";
-        thing = "";
+        keyPrefix = "examResult";
+        thing = "ExamResult";
         identifier = "";
+    }
+
+
+
+    public void checkParamsAddExamResult(Context ctx, List<String> params) throws ParameterError {
+
+    }
+
+    public void checkParamsGetExamResultEntries(Context ctx, List<String> params) throws ParameterError {
+
     }
 }
