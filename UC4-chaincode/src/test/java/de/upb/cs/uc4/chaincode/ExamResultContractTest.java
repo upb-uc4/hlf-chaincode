31000a648cc9e8ab6ed2759450239b3a9ea32025
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-12 18:43:37 +0100

    Add test for java.util.Date in Exam

--- UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/ExamResultContractTest.java
+++ UC4-chaincode/src/test/java/de/upb/cs/uc4/chaincode/ExamResultContractTest.java
@@ -5,6 +5,7 @@ import com.google.common.reflect.TypeToken;
 import de.upb.cs.uc4.chaincode.contract.examresult.ExamResultContract;
 import de.upb.cs.uc4.chaincode.contract.examresult.ExamResultContractUtil;
 import de.upb.cs.uc4.chaincode.contract.matriculationdata.MatriculationDataContract;
+import de.upb.cs.uc4.chaincode.contract.operation.OperationContract;
 import de.upb.cs.uc4.chaincode.helper.GsonWrapper;
 import de.upb.cs.uc4.chaincode.model.JsonIOTest;
 import de.upb.cs.uc4.chaincode.model.JsonIOTestSetup;
@@ -14,6 +15,7 @@ import de.upb.cs.uc4.chaincode.model.examresult.ExamResultEntry;
 import de.upb.cs.uc4.chaincode.util.TestUtil;
 import org.hyperledger.fabric.contract.Context;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import java.lang.reflect.Type;
@@ -27,6 +29,14 @@ public final class ExamResultContractTest extends TestCreationBase {
     private final ExamResultContract contract = new ExamResultContract();
     private final ExamResultContractUtil cUtil = new ExamResultContractUtil();
 
+    @Test
+    void simpleDateTest(){
+        String addExamParams = "[\"{\\\"examId\\\":\\\"\\\",\\\"courseId\\\":\\\"1b88275a-6d49-11eb-a92c-57114907e620\\\",\\\"moduleId\\\":\\\"Some Module ID\\\",\\\"lecturerEnrollmentId\\\":\\\"5634c3abc66e5e674ae67f551af04317a7e29498918701f121e3aaa500a270e0\\\",\\\"type\\\":\\\"Written Exam\\\",\\\"date\\\":\\\"2021-02-12T15:47:18.983Z\\\",\\\"ects\\\":5,\\\"admittableUntil\\\":\\\"2021-02-12T15:45:18.983Z\\\",\\\"droppableUntil\\\":\\\"2021-02-12T15:46:18.983Z\\\"}\"]";
+        Context ctx = TestUtil.mockContext(TestUtil.mockStub(new JsonIOTestSetup(), "initiateOperation"));
+        OperationContract contract = new OperationContract();
+        String result = contract.initiateOperation(ctx, "test-id", "UC4.Exam", "addExam", addExamParams);
+        System.out.println(result);
+    }
 
     String getTestConfigDir() {
         return "src/test/resources/test_configs/exam_result_contract";
